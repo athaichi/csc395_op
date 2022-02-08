a97a92c03742727eba4551868b2ad1ab9e795e1b
lanearohan <lanearohan@gmail.com>
2022-02-08 14:15:24 -0600

    Clean up and comment printing usable memory

--- kernel/boot.c
+++ kernel/boot.c
@@ -202,27 +202,35 @@ void kprintf(const char* format, ...) {
 }
 
 void usable_memory(struct stivale2_struct* hdr) {
-  kprint_s("Usable memory: \n    "); 
+  // print section label 
+  kprint_s("Usable memory: \n"); 
+
+  // find all the correct tags
   struct stivale2_struct_tag_kernel_base_address* base_address = find_tag(hdr, STIVALE2_STRUCT_TAG_KERNEL_BASE_ADDRESS_ID); 
   struct stivale2_struct_tag_memmap* memmap = find_tag(hdr, STIVALE2_STRUCT_TAG_MEMMAP_ID); 
   struct stivale2_struct_tag_hhdm* hhdm = find_tag(hdr, STIVALE2_STRUCT_TAG_HHDM_ID); 
 
+  // iterate through all mapped memory 
   for(uint64_t current = 0; current < memmap->entries; current++) {
     struct stivale2_mmap_entry cur = memmap->memmap[current]; 
-    if(cur.type == 1) { // section of memory is usable
-
+    
+    // if section of memory is usable
+    if(cur.type == 1) { 
       // print physical address 
+      kprint_s("    ");
       kprint_p(cur.base); 
       kprint_c('-'); 
       kprint_p(cur.base + cur.length); 
 
       kprint_s(" is mapped to "); 
 
+      // print virtual address
       kprint_p(cur.base - base_address->physical_base_address + hhdm->addr); 
       kprint_c('-'); 
       kprint_p(cur.base - base_address->physical_base_address + hhdm->addr + cur.length); 
+      
+      // format for next entry 
       kprint_c('\n');
-      kprint_s("    ");
     }
   }
 }
