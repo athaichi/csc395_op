ebba2b59bc68a3801800d6d4b5c72d3c184d6a50
lanearohan <lanearohan@gmail.com>
2022-02-10 14:55:28 -0600

    Maybe I've fixed the commits now?

---
+++ kernel/interrupts.c
@@ -0,0 +1,106 @@
+#include <stdint.h>
+#include <stddef.h>
+#include <stdarg.h>
+
+#include "stivale2.h"
+#include "util.h"
+#include "interrupts.h"
+
+// A struct the matches the layout of an IDT entry
+typedef struct idt_entry {
+  uint16_t offset_0;
+  uint16_t selector;
+  uint8_t ist : 3;
+  uint8_t _unused_0 : 5;
+  uint8_t type : 4;
+  uint8_t _unused_1 : 1;
+  uint8_t dpl : 2;
+  uint8_t present : 1;
+  uint16_t offset_1;
+  uint32_t offset_2;
+  uint32_t _unused_2;
+} __attribute__((packed)) idt_entry_t;
+
+typedef struct interrupt_context {
+  uintptr_t ip;
+  uint64_t cs;
+  uint64_t flags;
+  uintptr_t sp;
+  uint64_t ss;
+} __attribute__((packed)) interrupt_context_t;
+
+__attribute__((interrupt))
+void handler0(interrupt_context_t* ctx) {
+  kprintf("Divide Error\n");
+  halt();
+}
+
+// Make an IDT
+idt_entry_t idt[256];
+
+/**
+ * Set an interrupt handler for the given interrupt number.
+ *
+ * \param index The interrupt number to handle
+ * \param fn    A pointer to the interrupt handler function
+ * \param type  The type of interrupt handler being installed.
+ *              Pass IDT_TYPE_INTERRUPT or IDT_TYPE_TRAP from above.
+ */
+
+
+// set interrput handler for interrupt 0
+void idt_set_handler0(uint8_t index, void* fn, uint8_t type) {
+    idt->offset_0 = 0; 
+    idt->selector = IDT_CODE_SELECTOR; 
+    idt->ist = 0; 
+    idt->_unused_0 = 0; 
+    idt->offset_1 = 0; 
+    idt->offset_2 = 0; 
+    idt->dpl = 0; 
+
+    // call the actual handler
+    fn(); //somehow
+
+    // things to remember: bit masking/shifting
+    // write one interrput set handler for each 
+
+
+  // Fill in all fields of idt[index]
+  // Make sure you fill in:
+  //   handler (all three parts, which requires some bit masking/shifting)
+  //   type (using the parameter passed to this function)
+  //   p=1 (the entry is present)
+  //   dpl=0 (run the handler in kernel mode)
+  //   ist=0 (we aren't using an interrupt stack table, so just pass 0)
+  //   selector=IDT_CODE_SELECTOR
+  // =======
+  // handler
+
+  
+  
+}
+
+// This struct is used to load an IDT once we've set it up
+typedef struct idt_record {
+  uint16_t size;
+  void* base;
+} __attribute__((packed)) idt_record_t;
+
+/**
+ * Initialize an interrupt descriptor table, set handlers for standard exceptions, and install
+ * the IDT.
+ */
+void idt_setup() {
+  // Step 1: Zero out the IDT, probably using memset (which you'll have to implement)
+  // Write me!
+
+  // Step 2: Use idt_set_handler() to set handlers for the standard exceptions (1--21)
+  // Write me!
+
+  // Step 3: Install the IDT
+  idt_record_t record = {
+    .size = sizeof(idt),
+    .base = idt
+  };
+  __asm__("lidt %0" :: "m"(record));
+}
