a61126a8b7c484a081846c3ef96a1eefaf1210bd
lanearohan <lanearohan@gmail.com>
2022-02-10 14:23:57 -0600

    Begin interrupt handler

---
+++ kernel/interrupts.h
@@ -0,0 +1,12 @@
+#pragma once
+
+void idt_set_handler(uint8_t index, void* fn, uint8_t type)
+void idt_setup()
+
+// Every interrupt handler must specify a code selector. We'll use entry 5 (5*8=0x28), which
+// is where our bootloader set up a usable code selector for 64-bit mode.
+#define IDT_CODE_SELECTOR 0x28
+
+// IDT entry types
+#define IDT_TYPE_INTERRUPT 0xE
+#define IDT_TYPE_TRAP 0xF
