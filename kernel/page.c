8f9bfe66af4a912cb60c6da3adbaba31588a1360
lanearohan <lanearohan@gmail.com>
2022-02-16 19:41:04 -0600

    Create page table entry struct

---
+++ kernel/page.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+// create a page entry
+// should be 64 bits
+typedef struct page_table_entry {
+  bool present : 1;
+  bool writable : 1;
+  bool kernel : 1;
+  uint16_t unused : 9;
+  uint64_t address : 51;
+  bool no_execute : 1;
+} __attribute__((packed)) pt_entry_t;
+
+// 52 - 12 = 40
