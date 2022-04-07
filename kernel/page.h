185dab27c5a80ecc13b6bdd0c24e416b04ad923c
lanearohan <lanearohan@gmail.com>
2022-04-07 13:19:00 -0500

    Move paging stuff to page.c

--- kernel/page.h
+++ kernel/page.h
@@ -1,3 +1,18 @@
 #pragma once
+#include <stdbool.h>
+#include "stivale2.h"
+
+uintptr_t read_cr3();
+uintptr_t get_hhdm(struct stivale2_struct* hdr);
+void translate(void* address, struct stivale2_struct* hdr);
+
+uintptr_t pmem_alloc(); 
+void pmem_free(uintptr_t p); 
+
+void mem_init(struct stivale2_struct* hdr); 
+bool vm_map(uintptr_t root, uintptr_t address, bool usable, bool writable, bool executable); 
+bool vm_unmap(uintptr_t root, uintptr_t address); 
+bool vm_protect(uintptr_t root, uintptr_t address, bool user, bool writable, bool executable); 
+
 
 
