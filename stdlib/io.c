5f40ff037e6c945c8d530054a1617ddd34c7ef54
lanearohan <lanearohan@gmail.com>
2022-04-18 23:22:00 -0500

    Set up skeleton additional functions

--- stdlib/io.c
+++ stdlib/io.c
@@ -1,16 +1,17 @@
 #include "io.h"
 #include "../kernel/systemcalls.h"
 
-
-// long rc = syscall(SYS_READ, 0, buf, 5)
 long read(uint8_t filedes, char* buf, uint64_t numchars) {
     long rc = syscall(SYS_READ, filedes, buf, numchars); 
     return rc; 
 }
 
-// long rc2 = syscall(SYS_WRITE, 1, buf2, 6)
 long write(uint8_t filedes, char* str) {
     uint64_t len = strlen(str); 
     long rc = syscall(SYS_WRITE, filedes, str, len);
     return rc;  
 }
+
+// void printf(const char* format, ...) { }
+// char* getline() { }
+// long perror() { }
--- stdlib/io.h
+++ stdlib/io.h
@@ -6,9 +6,8 @@
 #define STDOUT 1
 #define STDERRROR 2
 
-
-// long rc = syscall(SYS_READ, 0, buf, 5)
 long read(uint8_t filedes, char* buf, uint64_t numchars); 
-
-// long rc2 = syscall(SYS_WRITE, 1, buf2, 6)
 long write(uint8_t filedes, char* str); 
+// void printf(const char* format, ...);
+// char* getline(); 
+// long perror(); 
